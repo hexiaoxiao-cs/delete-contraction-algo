2a714be9da0ac6d52eba99203e06196ee0f96d4e
Arist-Mac <xiaoxiao.he@rutgers.edu>
2020-04-27 15:57:13 -0400

    Kind of finished

--- src/algorithm/DeletionContractionAlgo.java
+++ src/algorithm/DeletionContractionAlgo.java
@@ -1,6 +1,7 @@
 package algorithm;
 
 import java.util.*;
+import java.math.*;
 
 import structure.Graph;
 import structure.Vertex;
@@ -81,16 +82,44 @@ public class DeletionContractionAlgo {
 		return !DFS_back(g2,v1,-1,v2);
 	}
 	
+	public static int get_Chi(Graph g,int t) {
+		
+		//if g does not have any edges
+		if(g.isEdge_Empty()==true) {
+			return (int)Math.pow((double)t,(double)g.vertices.size());
+		}
+		
+		int[] edge = g.get_edge_in_graph();
+		if(check_Cut(g,edge[0],edge[1])==true) {
+			return (t-1)*get_Chi(g.contraction(edge[0], edge[1]),t);
+		}
+		
+		return get_Chi(g.deletion(edge[0], edge[1]),t)-get_Chi(g.contraction(edge[0], edge[1]),t);
+	}
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int k=0;
 		Graph g = new Graph(4);
 		g.add_edge(0, 1);
 		g.add_edge(0, 3);
-		//g.add_edge(1, 2);
+		g.add_edge(1, 2);
 		g.add_edge(2, 3);
-		g.show_adj();
-		System.out.println(check_Loop(g,0,1));
-		System.out.println(check_Cut(g,0,1));
+//		g.add_edge(0, 2);
+//		g.add_edge(1, 3);
+		for(int i = 0; i <= g.num;i++) {
+			if(get_Chi(g,i)>0) {
+				k=i;
+				break;
+			}
+		}
+		System.out.println(k);
+//		g.show_adj();
+//		g.deletion(0, 1).show_adj();
+//		g.contraction(0, 2).show_adj();;
+//		System.out.println(check_Loop(g,0,1));
+//		System.out.println(check_Cut(g,0,1));
 //		ArrayList<Integer> returned=find_Loop(g);
 //		System.out.println(returned.toString());
 //		Graph g2=g.clone();
--- src/structure/Graph.java
+++ src/structure/Graph.java
@@ -23,6 +23,7 @@ public class Graph {
 			System.out.println("Does not contains "+v1+" or "+v2);
 			return -1;
 		}
+		if(v1==v2) {System.out.println("Adding self edge");return 0;}
 		return vertices.get(v1).add_edge(v2) + vertices.get(v2).add_edge(v1);
 		
 	}
@@ -32,8 +33,10 @@ public class Graph {
 			System.out.println("Does not contains "+v1+" or "+v2);
 			return -1;
 		}
+		if(v1==v2) {System.out.println("Deleting self edge");return 0;}
 		return vertices.get(v1).delete_edge(v2) + vertices.get(v2).delete_edge(v1);
 	}
+	
 	
 	public Graph clone() {
 		Graph g=new Graph();
@@ -53,8 +56,42 @@ public class Graph {
 	}
 	
 	public Graph contraction(int v1, int v2) {
-		return null;
+		Graph g = this.clone();
+		ArrayList<Integer> temp = new ArrayList();
+		for(int a : g.vertices.get(g.vertices.indexOf(new Vertex(v2))).edge) {//connecting all edges in v2 to v1
+			//g.vertices.get(g.vertices.indexOf(new Vertex(v1))).add_edge(a);//add edge
+			g.add_edge(v1, a);
+			temp.add(a);
+		}
+		for(int a : temp) {
+			g.delete_edge(v2,a);
+		}
+		g.delete_edge(v1, v2);
+		g.vertices.remove(g.vertices.indexOf(new Vertex(v2)));
+		for(Vertex v : g.vertices) {
+			if(v.num>v2) {v.num--;}
+			for(int k  = 0; k < v.edge.size();k++) {
+				if(v.edge.get(k)>v2) {v.edge.set(k, v.edge.get(k)-1);}
+			}
+		}
+		g.num--;
+		return g;
 		
+	}
+	
+	public boolean isEdge_Empty() {
+		for(Vertex v : this.vertices) {
+			if(v.edge.isEmpty()==false) {return false;
+			}
+		}
+		return true;
+	}
+	public int[] get_edge_in_graph() {
+		Random rand = new Random();
+		int a = rand.nextInt(this.vertices.size());
+		int b = rand.nextInt(this.vertices.get(a).edge.size());
+		int[] res={a,this.vertices.get(a).edge.get(b)};
+		return res;
 	}
 	
 	public void show_adj() {
